c281a901fedb8a8de52308e6c1b78c846dfba367
ferb96 <fduy96@gmail.com>
2018-08-28 13:57:11 -0500

    working

--- app.js
+++ app.js
@@ -2,39 +2,93 @@
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
+var randomGreek = require('random-greek-gods');
+var math = require('mathjs');
 
 // this is the whole app
 var app = express();
 
-// api
-var api = require('./api/index.js');
+// websocket server
+var expressWs = require('express-ws')(app);
 
 // this server gonna run on this port
 app.set('port', 8080);
+
+// global variables
+history = [];
+clients = [];
 
 // pug settings
 app.set('views','./views');
 app.set('view engine', 'pug');
 
-// logging out all the access
+// verbose logging
 app.use(function(req, res, next){
 	console.log(req.method, req.url);
 	next();
 });
 
-// global variables
-app.locals.history = [];
+// websocket functions
+expressWs.getWss().on('connection', function(ws) {
+	// on new connection: generate index and id
+	var index=clients.push(ws) - 1;
+	var id=randomGreek();
+	ws.send('id='+id); // let the client know their id
+	console.log('connected:',id,index);
+	// broadcast current history
+	for (var i=0; i < history.length; i++){
+		ws.send(history[i].id + ': ' + history[i].eq + ' result=' + history[i].result);
+	}
+
+	// on message
+	ws.on('message', function incoming(eq) {
+		// calculate
+		result="";
+		error="";
+		try {
+			result=math.eval(eq);
+		}
+		catch (err) {
+			result='ERR';
+			error=err.message;
+		}
+
+		if (error!=""){
+			// send error
+			ws.send('ERR! '+error);
+		} else {
+			// put in history
+			entry = {
+				id: id,
+				eq: eq,
+				result: result
+			}
+			history.push(entry);
+			// check bounds
+			if (history.length>10) history.shift();
+			// broadcast msg to everyone
+			for (var i=0; i < clients.length; i++){
+				clients[i].send(id + ': ' + eq + ' result=' + result);
+			}
+		}
+	});
+
+	// on close connection
+	ws.on('close', function closing(code, reason) {
+		console.log('disconnected:',id,index);
+		// remove from client lists
+		clients.splice(index,1);
+	});
+});
+
+app.ws('/ws', function(ws, req) {
+	// dont remove this
+});
 
 // static directory
 app.use(express.static(path.join(__dirname, 'static')));
 // node modules
 app.use('/node_modules', express.static(path.join(__dirname,'/node_modules')));
-// enable parser so it can read POST
-app.use(bodyParser.urlencoded({ extended: false }));;
-app.use(bodyParser.json());
-
-// api
-app.use('/api', api);
 
 // front end
 app.get('/', function(req,res){
--- static/frontend.js
+++ static/frontend.js
@@ -1,13 +1,174 @@
-// when pressing enter in searchbox
-$("#eq").on('keyup', function (e) {
-	if (e.keyCode == 13) {
-		sendEQ();
+$(function() {
+	$('#eq').focus();
+	id = "";
+
+	// if user is running mozilla then use it's built-in WebSocket
+	window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+	// if browser doesn't support WebSocket, show notification
+
+	// open connection
+	var connection = new WebSocket('ws://138.236.26.111:8080/ws');
+	connection.onopen = function() {
+
+	}
+	connection.onmessage = function(msg){
+		data=msg.data
+		if (data.slice(0,3) == "id="){
+			id = data.slice(3);
+			$("#idd").html('your id is '+id);
+		} else if (data.slice(0,3) == "ERR"){
+			$('#history').append('<div class="entry error">'+data+'</div>');
+		} else if (data.slice(0,id.length) == id){
+			$('#history').append('<div class="entry own">'+data+'</div>');
+		} else {
+			$('#history').append('<div class="entry">'+data+'</div>');
+		}
+		var count = $("#history div").length;
+		if (count > 10) $("#history div").first().remove();
+	}
+
+	$("#eq").on('keyup', function (e) {
+		if (e.keyCode == 13) {
+			sendEQ();
+		}
+	});
+
+	// when search button is clicked, send a request to spotify
+	$('#submit').click(sendEQ);
+
+	function sendEQ(){
+		console.log('sending equation to sv');
+		var eq=$('#eq').val();
+		$('#eq').val('');
+		$('#eq').focus();
+		connection.send(eq);
 	}
 });
 
-// when search button is clicked, send a request to spotify
-$('#submit').click(sendEQ);
-
-function sendEQ(){
-	console.log('sending equation to sv');
+function prefill(ss){
+	$("#eq").val(ss);
+	$('#eq').focus();
 }
+
+// when pressing enter in searchbox
+
+
+
+
+// $(function () {
+//   "use strict";
+//   // for better performance - to avoid searching in DOM
+//   var content = $('#content');
+//   var input = $('#input');
+//   var status = $('#status');
+//   // my color assigned by the server
+//   var myColor = false;
+//   // my name sent to the server
+//   var myName = false;
+//   // if user is running mozilla then use it's built-in WebSocket
+//   window.WebSocket = window.WebSocket || window.MozWebSocket;
+//   // if browser doesn't support WebSocket, just show
+//   // some notification and exit
+//   if (!window.WebSocket) {
+//     content.html($('<p>',
+//       { text:'Sorry, but your browser doesn\'t support WebSocket.'}
+//     ));
+//     input.hide();
+//     $('span').hide();
+//     return;
+//   }
+//   // open connection
+//   var connection = new WebSocket('ws://127.0.0.1:1337');
+//   connection.onopen = function () {
+//     // first we want users to enter their names
+//     input.removeAttr('disabled');
+//     status.text('Choose name:');
+//   };
+//   connection.onerror = function (error) {
+//     // just in there were some problems with connection...
+//     content.html($('<p>', {
+//       text: 'Sorry, but there\'s some problem with your '
+//          + 'connection or the server is down.'
+//     }));
+//   };
+//   // most important part - incoming messages
+//   connection.onmessage = function (message) {
+//     // try to parse JSON message. Because we know that the server
+//     // always returns JSON this should work without any problem but
+//     // we should make sure that the massage is not chunked or
+//     // otherwise damaged.
+//     try {
+//       var json = JSON.parse(message.data);
+//     } catch (e) {
+//       console.log('Invalid JSON: ', message.data);
+//       return;
+//     }
+//     // NOTE: if you're not sure about the JSON structure
+//     // check the server source code above
+//     // first response from the server with user's color
+//     if (json.type === 'color') { 
+//       myColor = json.data;
+//       status.text(myName + ': ').css('color', myColor);
+//       input.removeAttr('disabled').focus();
+//       // from now user can start sending messages
+//     } else if (json.type === 'history') { // entire message history
+//       // insert every single message to the chat window
+//       for (var i=0; i < json.data.length; i++) {
+//       addMessage(json.data[i].author, json.data[i].text,
+//           json.data[i].color, new Date(json.data[i].time));
+//       }
+//     } else if (json.type === 'message') { // it's a single message
+//       // let the user write another message
+//       input.removeAttr('disabled'); 
+//       addMessage(json.data.author, json.data.text,
+//                  json.data.color, new Date(json.data.time));
+//     } else {
+//       console.log('Hmm..., I\'ve never seen JSON like this:', json);
+//     }
+//   };
+//   /**
+//    * Send message when user presses Enter key
+//    */
+//   input.keydown(function(e) {
+//     if (e.keyCode === 13) {
+//       var msg = $(this).val();
+//       if (!msg) {
+//         return;
+//       }
+//       // send the message as an ordinary text
+//       connection.send(msg);
+//       $(this).val('');
+//       // disable the input field to make the user wait until server
+//       // sends back response
+//       inpu.attr('disabled', 'disabled');
+//       // we know that the first message sent from a user their name
+//       if (myName === false) {
+//         myName = msg;
+//       }
+//     }
+//   });
+//   /**
+//    * This method is optional. If the server wasn't able to
+//    * respond to the in 3 seconds then show some error message 
+//    * to notify the user that something is wrong.
+//    */
+//   setInterval(function() {
+//     if (connection.readyState !== 1) {
+//       status.text('Error');
+//       input.attr('disabled', 'disabled').val(
+//           'Unable to communicate with the WebSocket server.');
+//     }
+//   }, 3000);
+//   /**
+//    * Add message to the chat window
+//    */
+//   function addMessage(author, message, color, dt) {
+//     content.prepend('<p><span style="color:' + color + '">'
+//         + author + '</span> @ ' + (dt.getHours() < 10 ? '0'
+//         + dt.getHours() : dt.getHours()) + ':'
+//         + (dt.getMinutes() < 10
+//           ? '0' + dt.getMinutes() : dt.getMinutes())
+//         + ': ' + message + '</p>');
+//   }
+// });
